ff82618f05b78746aedbf4153e404c3a180fad99
Omar Yasser <anaesthesiayassermadi717@gmail.com>
2025-07-05 17:42:34 +0300

    edition1

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,15 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        // 1. إنشاء العميل
-        Customer customer = new Customer(1, "Omar", 2000); // معاه 1000 جنيه
 
-        // 2. المنتجات
+        Customer customer = new Customer(1, "Omar", 2000);
+
+
         ShippableProduct cheese = new ShippableProduct(1, "Cheese", 100, 5, LocalDate.of(2025, 12, 31), 400); // 400g
-        ShippableProduct tv = new ShippableProduct(2, "TV", 200, 3, null, 7000); // 7kg, لا ينتهي صلاحيته
-        Product scratchCard = new Product(3, "Scratch Card", 50, 10, null); // مش قابل للشحن
+        ShippableProduct tv = new ShippableProduct(2, "TV", 200, 3, null, 7000); // 7kg
+        Product scratchCard = new Product(3, "Scratch Card", 50, 10, null);
 
-        // 3. الكارت
+
         Cart cart = new Cart();
         cart.add(cheese, 2);        // 2x Cheese = 200
         cart.add(tv, 3);            // 3x TV = 600
